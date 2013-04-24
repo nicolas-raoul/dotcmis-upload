3cd89b5dcacd8acf0045e2bc30aedc0c77c52dfb
Nicolas Raoul <nicolas.raoul@gmail.com>
2013-04-24 17:52:08 +0900

    Wrote from scratch and tested successfully

---
+++ dotcmis-issue632/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DotCMIS;
+using DotCMIS.Client.Impl;
+using DotCMIS.Client;
+using System.IO;
+using DotCMIS.Data.Impl;
+
+/**
+ * Code showing DotCMIS issue 632.
+ * Output:
+ * 
+ * before
+ * after
+ * before
+ * [program gets stuck here]
+ *
+ */
+namespace dotcmis_issue632
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create session.
+            var parameters = new Dictionary<string, string>();
+            parameters[SessionParameter.BindingType] = BindingType.AtomPub;
+            parameters[SessionParameter.AtomPubUrl] = "http://192.168.0.22:8080/alfresco/cmisatom";
+            parameters[SessionParameter.User] = "admin";
+            parameters[SessionParameter.Password] = "admin";
+            var factory = SessionFactory.NewInstance();
+            ISession session = factory.GetRepositories(parameters)[0].CreateSession();
+
+            // Update a document twice.
+            Document doc = (Document)session.GetObjectByPath("/User Homes/test.txt");
+            UpdateFile(doc);
+            UpdateFile(doc); // This one never returns. 
+        }
+
+        private static void UpdateFile(IDocument doc)
+        {
+            Stream data = File.OpenRead("../../local.txt");
+
+            ContentStream contentStream = new ContentStream();
+            contentStream.FileName = doc.ContentStreamFileName;
+            contentStream.Length = data.Length;
+            contentStream.MimeType = "application/octet-stream";
+            contentStream.Stream = data;
+            contentStream.Stream.Flush();
+
+            Console.WriteLine("before");
+            doc.SetContentStream(contentStream, true, true);
+            Console.WriteLine("after"); // Not reached the second time the method is called.
+
+            data.Close();
+            data.Dispose();
+            contentStream.Stream.Close();
+        }
+    }
+}
