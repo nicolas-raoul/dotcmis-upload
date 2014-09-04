194f82cbecfe3ff00d442ccb256ffad104034239
Nicolas Raoul <nicolas.raoul@gmail.com>
2014-09-04 15:07:26 +0900

    Implemented upload

--- dotcmis-upload/Program.cs
+++ dotcmis-upload/Program.cs
@@ -9,16 +9,11 @@ using System.IO;
 using DotCMIS.Data.Impl;
 
 /**
- * Code demonstrating DotCMIS issue 632.
- * Modify AtomPubUrl, User, Password, remoteFilePath
- * Output:
- * 
- * before
- * after
- * before
- * [program gets stuck here]
- *
+ * Upload documents to a CMIS server using DotCMIS.
  */
+using System.Threading;
+
+
 namespace dotcmis_upload
 {
     class Program
@@ -28,37 +23,47 @@ namespace dotcmis_upload
             // Create session.
             var parameters = new Dictionary<string, string>();
             parameters[SessionParameter.BindingType] = BindingType.AtomPub;
-            parameters[SessionParameter.AtomPubUrl] = "http://192.168.0.22:8080/alfresco/cmisatom"; // MODIFY HERE
+            parameters[SessionParameter.AtomPubUrl] = "http://192.168.0.67:8080/alfresco/cmisatom"; // MODIFY HERE
             parameters[SessionParameter.User] = "admin"; // MODIFY HERE
             parameters[SessionParameter.Password] = "admin"; // MODIFY HERE
             var factory = SessionFactory.NewInstance();
             ISession session = factory.GetRepositories(parameters)[0].CreateSession();
 
-            // Update a document twice.
-            string remoteFilePath = "/User Homes/test.txt";
-            Document doc = (Document)session.GetObjectByPath(remoteFilePath);
-            Update(doc);
-            Update(doc); // This one never returns. 
+			IFolder folder =  (IFolder) session.GetObjectByPath("/Sites/site1/documentLibrary/upload");
+
+			while (true)
+			{
+				UploadRandomDocumentTo(folder);
+				Thread.Sleep(1000);
+			}
         }
 
-        private static void Update(IDocument doc)
+		private static void UploadRandomDocumentTo(IFolder folder)
         {
-            Stream data = File.OpenRead("../../local.txt");
+			string filename = "file_" + Guid.NewGuid() + ".bin";
 
-            ContentStream contentStream = new ContentStream();
-            contentStream.FileName = doc.ContentStreamFileName;
-            contentStream.Length = data.Length;
-            contentStream.MimeType = "application/octet-stream";
-            contentStream.Stream = data;
-            contentStream.Stream.Flush();
+			//byte[] content = UTF8Encoding.UTF8.GetBytes("Hello World!");
+			int sizeInMb = 4;
+			byte[] data = new byte[sizeInMb * 1024 * 1024];
+			Random rng = new Random();
+			rng.NextBytes(data);
 
-            Console.WriteLine("before");
-            doc.SetContentStream(contentStream, true, true);
-            Console.WriteLine("after"); // Not reached the second time the method is called.
+			IDictionary<string, object> properties = new Dictionary<string, object>();
+			properties[PropertyIds.Name] = filename;
+			properties[PropertyIds.ObjectTypeId] = "cmis:document";
 
-            data.Close();
-            data.Dispose();
-            contentStream.Stream.Close();
+			ContentStream contentStream = new ContentStream();
+			contentStream.FileName = filename;
+			contentStream.MimeType = "application/octet-stream";
+			contentStream.Length = data.Length;
+			contentStream.Stream = new MemoryStream(data);
+
+			IDocument doc = folder.CreateDocument(properties, contentStream, null);
+
+			contentStream.Stream.Close();
+			contentStream.Stream.Dispose();
+
+			Console.WriteLine ("Uploaded " + filename);
         }
     }
 }
